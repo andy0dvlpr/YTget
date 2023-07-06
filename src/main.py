6ca8dc84492aa3061075ad092aec9d60ec70bc08
andy0dvlpr <37957771+andy0dvlpr@users.noreply.github.com>
2023-07-06 16:57:00 +0300

    Add logo

--- src/main.py
+++ src/main.py
@@ -42,6 +42,19 @@ helpmenu.add_command(label="About YTget", command=aboutWindowOpen)
 menubar.add_cascade(label="Help", menu=helpmenu)
 win.config(menu=menubar)
 
+# Logo
+fYTgetLogos = tk.Frame(win)
+YTgetLogo = ImageTk.PhotoImage(Image.open("images/icon-90p.png"))
+lYTgetLogo = tk.Label(fYTgetLogos, image=YTgetLogo)
+YTgetTextLogo = ImageTk.PhotoImage(Image.open("images/Ytget-transparent.png"))
+lYTgetTextLogo = tk.Label(fYTgetLogos, image=YTgetTextLogo)
+
+# Grid and Pack cannot be used in the same frame. Due to the fact that the main
+# window is also treated as a frame, I've decided to put the logo using pack inside
+# the main window, and everything else using grid inside of a big frame titled "w".
+w = tk.Frame(win)
+w.option_add("*font", "Segoe")
+
 # Display a message if the link is invalid, or is a playlist.
 def validateLink(*args):
     insertedLink = linkEntry.get()
@@ -54,25 +67,25 @@ def validateLink(*args):
         validTextString.set("This doesn't seem to be a valid link.")
         validLabel.config(fg="red")
 
-linkLabel = tk.Label(win, text="Link to video:")
+linkLabel = tk.Label(w, text="Link to video:")
 linkVar = tk.StringVar()
-linkEntry = tk.Entry(win, textvariable=linkVar, width=69) #nice
+linkEntry = tk.Entry(w, textvariable=linkVar, width=69) #nice
 linkVar.trace("w", validateLink)
 validTextString = tk.StringVar() # Used by validateLink(), which is called everytime the Entry is modified.
-validLabel = tk.Label(win, textvariable=validTextString)
+validLabel = tk.Label(w, textvariable=validTextString)
 # Put a bold font on the text.
 bold_font = font.Font(validLabel, validLabel.cget("font"))
 bold_font.configure(weight="bold")
 validLabel.config(font=bold_font)
 
-formatFrame = tk.Frame(win)
+formatFrame = tk.Frame(w)
 formatLabel = tk.Label(formatFrame, text="Format:")
 downloadFormat = tk.StringVar()
 audioButton = tk.Radiobutton(formatFrame, text="Audio", variable=downloadFormat, value="audio")
 audioButton.select() # We need to have a default option selected.
 videoButton = tk.Radiobutton(formatFrame, text="Video", variable=downloadFormat, value="video")
 
-DLFrame = tk.Frame(win)
+DLFrame = tk.Frame(w)
 DLLabel = tk.Label(DLFrame, text="Download location:")
 DLOptions = ["Default location", "Choose another location..."]
 DLSel = tk.StringVar()
@@ -86,7 +99,11 @@ downloadLocation = tk.OptionMenu(DLFrame, DLSel, *DLOptions, command=changeDL)
 downloadLocation.config(width=30, anchor="w", padx=10, indicatoron=0)
 
 ## GEOMETRY ##
+fYTgetLogos.grid(column=1, row=1)
+lYTgetLogo.pack(side="left")
+lYTgetTextLogo.pack(side="right")
 
+w.grid(column=1, row=2)
 linkLabel.grid(row=1, column=1, sticky="w")
 linkEntry.grid(row=2, column=1, columnspan=2)
 validLabel.grid(row=3, column=1, columnspan=2, sticky="w")
